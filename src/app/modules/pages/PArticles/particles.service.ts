2dfc717ac5ef292d20af7e742b14d84f63dd2379
miakeous <tardiveau.pierre@telecom-st-etienne.fr>
2019-02-10 14:47:08 +0100

    hotFix on storage

--- src/app/modules/pages/PArticles/particles.service.ts
+++ src/app/modules/pages/PArticles/particles.service.ts
@@ -10,26 +10,23 @@ import {catchError} from "rxjs/internal/operators";
 @Injectable()
 export class ParticlesService  {
 
-  public resultat:any=[];
+  public resultat2:any=[];
   constructor( public storage: Storage) {
 
   }
 
   getArticles(){
-  this.storage.keys().then((k)=>{
-    this.storage.get(k).then((val)=>{
-     //console.log(val);
-     let id = val.id;
-     let userId = val.userid;
-     let title = val.title;
-     let body = val.body;
-     let value = {id:id,userId : userId, title: title, body:body};
-    // console.log(value);
-     this.resultat.push(value);
-    });
-  });
-  //console.log(this.resultat);
-    return this.resultat;
+    this.storage.forEach(value => {
+      let id = value.id;
+      let userId = value.userid;
+      let title = value.title;
+      let body = value.body;
+      let value2 = {id:id,userId : userId, title: title, body:body};
+      // console.log(value);
+      this.resultat2.push(value2);
+      }
+    );
+    return this.resultat2;
   }
 
 
